ad1b82c8a292723b34573175d630c7c3bec0b05a
Ayman-Kz25 <kamalakz786@gmail.com>
2026-02-27 00:30:47 +0500

    commit 4

--- client/src/components/StoryViewer.jsx
+++ client/src/components/StoryViewer.jsx
@@ -1,13 +1,38 @@
-const StoryViewer = ({viewStory, setViewStory}) => {
-  return (
-    <div className="view-story-container" style={{backgroundColor: viewStory.media_type === "text" ? viewStory.bg_clr : '#000000'}}>
-        {/* Progress Bar */}
-        <div className="progress-bar">
-            <div className="progress-line" style={{width: '50'}}>
+import { BadgeCheck, X } from "lucide-react";
 
-            </div>
+const StoryViewer = ({ viewStory, setViewStory }) => {
+    const handleClose = () => {
+        setViewStory(null)
+    }
+  return (
+    <div
+      className="view-story-container"
+      style={{
+        backgroundColor:
+          viewStory.media_type === "text" ? viewStory.bg_clr : "#000000",
+      }}
+    >
+      {/* Progress Bar */}
+      <div className="progress-bar">
+        <div className="progress-line" style={{ width: "50" }}></div>
+      </div>
+      {/* User Info - Top Left */}
+      <div className="user-info">
+        <img
+          src={viewStory.user?.profile_pic}
+          alt=""
+          className="size-7 sm:size-8 rounded-full object-cover border border-gray-50"
+        />
+        <div className="text-gray-50 font-medium flex items-center gap-1.5">
+            <span>{viewStory.user?.full_name}</span>
+            <BadgeCheck size={18}/>
         </div>
+      </div>
+      {/* Close Button */}
+      <button className="close-btn" onClick={handleClose}>
+        <X className="close-icon"/>
+      </button>
     </div>
-  )
-}
-export default StoryViewer
+  );
+};
+export default StoryViewer;
