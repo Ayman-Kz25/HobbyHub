99f716b0093335c5fd5320d38741e3b02649fde1
Ayman-Kz25 <kamalakz786@gmail.com>
2026-02-27 00:00:32 +0500

    commit 3

--- client/src/components/StoryViewer.jsx
+++ client/src/components/StoryViewer.jsx
@@ -1,7 +1,12 @@
-const StoryViewer = () => {
+const StoryViewer = ({viewStory, setViewStory}) => {
   return (
-    <div>
-        
+    <div className="view-story-container" style={{backgroundColor: viewStory.media_type === "text" ? viewStory.bg_clr : '#000000'}}>
+        {/* Progress Bar */}
+        <div className="progress-bar">
+            <div className="progress-line" style={{width: '50'}}>
+
+            </div>
+        </div>
     </div>
   )
 }
--- client/src/components/Storybar.jsx
+++ client/src/components/Storybar.jsx
@@ -3,6 +3,7 @@ import { storyData } from "../data/data";
 import { Plus } from "lucide-react";
 import moment from "moment";
 import StoryModal from "./StoryModal";
+import StoryViewer from "./StoryViewer";
 
 const Storybar = () => {
   const [stories, setStories] = useState([]);
@@ -20,7 +21,7 @@ const Storybar = () => {
     <div className="storybar-container">
       <div className="flex gap-4 pb-5">
         {/* Add New Story Card */}
-        <div onClick={()=>setShowModal(true)} className="new-story-card">
+        <div onClick={() => setShowModal(true)} className="new-story-card">
           <div className="h-full flex flex-col items-center justify-center p-4">
             <div className="add-icon">
               <Plus className="w-5 h-5 text-gray-50" strokeWidth={3} />
@@ -32,14 +33,19 @@ const Storybar = () => {
         </div>
         {/* Story Cards */}
         {stories.map((story, index) => (
-          <div key={index} className="story-card">
+          <div
+            key={index}
+            className="story-card"
+            style={{backgroundColor: story.bg_clr}}
+            onClick={() => setViewStory(story)}
+          >
             <img src={story.user.profile_pic} alt="" className="profile-pic" />
             <p className="content">{story.content}</p>
             <p className="creation-datetime">
               {moment(story.createdAt).fromNow()}
             </p>
             {story.media_type !== "text" && (
-              <div className="absolute inset-0 z-1 rounded-lg bg-black overflow-hidden">
+              <div className="absolute inset-0 z-1 rounded-lg overflow-hidden bg-black">
                 {story.media_type === "image" ? (
                   <img src={story.media_url} alt="" className="story-img" />
                 ) : (
@@ -54,6 +60,11 @@ const Storybar = () => {
       {/* Add Story Modal */}
       {showModal && (
         <StoryModal setShowModal={setShowModal} fetchStories={fetchStories} />
+      )}
+
+      {/* View Story Modal */}
+      {viewStory && (
+        <StoryViewer viewStory={viewStory} setViewStory={setViewStory} />
       )}
     </div>
   );
