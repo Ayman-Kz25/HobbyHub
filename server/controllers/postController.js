80b17ba5ff5fb1233a36e705463a9cb5fcef075a
Ayman-Kz25 <kamalakz786@gmail.com>
2026-03-04 09:46:24 +0500

    commit 45

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -13,8 +13,10 @@ export const addPost = async (req, res) => {
     let media_urls = [];
 
     if (media.length > 0) {
+
       media_urls = await Promise.all(
         media.map(async (item) => {
+
           const file = fs.readFileSync(item.path);
           const base64File = file.toString("base64");
 
@@ -24,12 +26,15 @@ export const addPost = async (req, res) => {
             folder: "posts",
           });
 
-          return imagekit.url({
-            path: response.filePath,
+          return imagekit.helper.buildSrc({
+            urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+            src: response.filePath,
             transformation: [
-              { quality: "auto" },
-              { format: "webp" },
-              { width: 1280 },
+              {
+                quality: "auto",
+                format: "webp",
+                width: 1280,
+              },
             ],
           });
         }),
@@ -58,12 +63,11 @@ export const getFeedPosts = async (req, res) => {
 
     const userIds = [userId, ...user.friends, ...user.following];
     const posts = await Post.find({
-      user: { $in: userIds },
-    })
-      .populate("user")
-      .sort({ createdAt: -1 });
+      user: {$in: userIds}
+    }).populate('user').sort({createdAt: -1});
 
-    res.json({ succes: true, posts });
+    res.json({succes: true, posts});
+
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
@@ -74,20 +78,21 @@ export const getFeedPosts = async (req, res) => {
 export const likePost = async (req, res) => {
   try {
     const { userId } = req.auth();
-    const { postId } = req.body;
+    const {postId} = req.body;
 
     const post = await Post.findById(postId);
 
-    if (post.likes_count.includes(userId)) {
-      post.likes_count = post.likes_count.filter((user) => user !== userId);
+    if(post.likes_count.includes(userId)){
+      post.likes_count = post.likes_count.filter(user => user !== userId);
       await post.save();
 
-      res.json({ success: true, message: "Post unliked" });
+      res.json({success: true, message: 'Post unliked'});
     } else {
       post.likes_count.push(userId);
       await post.save();
-      res.json({ success: true, message: "Post liked" });
+      res.json({success: true, message: 'Post liked'});
     }
+
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
